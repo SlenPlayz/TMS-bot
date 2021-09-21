c6619dc07926a6dd51d5334333daf411d2bccdfd
SlenPlayz <67079195+SlenPlayz@users.noreply.github.com>
2021-09-21 13:12:50 +0000

    !periodic-table

--- index.js
+++ index.js
@@ -2,26 +2,41 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-	console.log(`Logged in as ${client.user.tag}!`);
+  console.log(`Logged in as ${client.user.tag}!`);
 })
 client.on('message', msg => {
-  switch(msg.content){
-    
+  switch (msg.content) {
+    case '!periodic-table':
+      msg.channel.send(`https://media.discordapp.net/attachments/879257283337547827/889859296165265428/Periodic-Table-O-Level-2020-1332x666.jpg`)
+      break;
   }
-  if(msg.content.substring(0,4) === '!say'){
-      if (msg.member.roles.cache.find(role => role.name == "Mods")) {
-        if(msg.content.substring(5) != '') {
+  if (msg.content.substring(0, 4) === '!say') {
+    if (msg.member.roles.cache.find(role => role.name == "Mods")) {
+      if (msg.content.substring(5) != '') {
         msg.delete()
         msg.channel.send(msg.content.substring(5));
-        }
-        else{
-          msg.reply('Enter a message for me to repeat')
-        }
       }
-      else{
-        msg.reply('You dont have permission to use that command')
+      else {
+        msg.reply('Enter a message for me to repeat')
       }
     }
+    else {
+      msg.reply('You dont have permission to use that command')
+    }
+  }
+  if (msg.content.substring(0, 5) === '!eval') {
+    if (msg.author.id = "517191312328491039") {
+      try {
+        eval(msg.content.substring(6));
+        msg.channel.send('Done!')
+      } catch (err) {
+        msg.channel.send('Error!')
+      }
+    }
+    else {
+      msg.reply('You dont have permission to use that command')
+    }
+  }
 })
 
 client.login(process.env.DISCORD_TOKEN);
