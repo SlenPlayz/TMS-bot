481c50e3b728ca9499c683a28467b55aef756e36
SlenPlayz <67079195+SlenPlayz@users.noreply.github.com>
2021-10-03 06:50:54 +0000

    e

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setActivity({ name: 'Good luck for EOY! You can do it!!!', type: 'PLAYING'});
 })
 client.on('message', msg => {
   switch (msg.content) {
